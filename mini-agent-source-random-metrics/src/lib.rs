dfd31c8bcb65cbd961bcc5551760852dea6338f8
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-12-13 20:59:16 +0100

    refactor: rename timer source to random metrics
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- mini-agent-source-random-metrics/src/lib.rs
+++ mini-agent-source-random-metrics/src/lib.rs
@@ -4,27 +4,27 @@ use mini_agent_source_prelude::timer;
 use tokio::sync::mpsc;
 
 #[derive(Debug, serde::Deserialize)]
-pub struct TimerConfig {
+pub struct RandomMetricsConfig {
     pub interval: f64,
 }
 
-impl SourceConfig for TimerConfig {
-    type Output = Timer;
+impl SourceConfig for RandomMetricsConfig {
+    type Output = RandomMetrics;
 
     fn build(self, output: mpsc::Sender<Event>) -> Self::Output {
-        Timer {
+        RandomMetrics {
             interval: tokio::time::interval(tokio::time::Duration::from_secs_f64(self.interval)),
             output,
-            executor: TimerExecutor,
+            executor: RandomMetricsExecutor,
         }
     }
 }
 
-pub type Timer = timer::Timer<TimerExecutor>;
+pub type RandomMetrics = timer::Timer<RandomMetricsExecutor>;
 
-pub struct TimerExecutor;
+pub struct RandomMetricsExecutor;
 
-impl timer::Executor for TimerExecutor {
+impl timer::Executor for RandomMetricsExecutor {
     async fn execute(&mut self, output: mpsc::Sender<Event>) {
         if let Err(err) = output
             .send(Event::Metric(EventMetric::now("instant", 0.0)))
--- mini-agent/src/source/mod.rs
+++ mini-agent/src/source/mod.rs
@@ -4,8 +4,8 @@ use tokio::sync::mpsc::Sender;
 pub enum Source {
     HttpServer(mini_agent_source_http_server::HttpServer),
     RandomLogs(mini_agent_source_random_logs::RandomLogs),
+    RandomMetrics(mini_agent_source_random_metrics::RandomMetrics),
     Sysinfo(mini_agent_source_sysinfo::Sysinfo),
-    Timer(mini_agent_source_timer::Timer),
 }
 
 impl mini_agent_core::prelude::Component for Source {
@@ -13,8 +13,8 @@ impl mini_agent_core::prelude::Component for Source {
         match self {
             Self::HttpServer(inner) => inner.run().await,
             Self::RandomLogs(inner) => inner.run().await,
+            Self::RandomMetrics(inner) => inner.run().await,
             Self::Sysinfo(inner) => inner.run().await,
-            Self::Timer(inner) => inner.run().await,
         }
     }
 }
@@ -27,9 +27,9 @@ impl From<mini_agent_source_http_server::HttpServer> for Source {
     }
 }
 
-impl From<mini_agent_source_timer::Timer> for Source {
-    fn from(value: mini_agent_source_timer::Timer) -> Self {
-        Self::Timer(value)
+impl From<mini_agent_source_random_metrics::RandomMetrics> for Source {
+    fn from(value: mini_agent_source_random_metrics::RandomMetrics) -> Self {
+        Self::RandomMetrics(value)
     }
 }
 
@@ -44,8 +44,8 @@ impl From<mini_agent_source_sysinfo::Sysinfo> for Source {
 pub(crate) enum SourceConfig {
     HttpServer(mini_agent_source_http_server::HttpServerConfig),
     RandomLogs(mini_agent_source_random_logs::RandomLogsConfig),
+    RandomMetrics(mini_agent_source_random_metrics::RandomMetricsConfig),
     Sysinfo(mini_agent_source_sysinfo::SysinfoConfig),
-    Timer(mini_agent_source_timer::TimerConfig),
 }
 
 impl mini_agent_source_prelude::prelude::SourceConfig for SourceConfig {
@@ -55,8 +55,8 @@ impl mini_agent_source_prelude::prelude::SourceConfig for SourceConfig {
         match self {
             Self::HttpServer(inner) => Source::HttpServer(inner.build(sender)),
             Self::RandomLogs(inner) => Source::RandomLogs(inner.build(sender)),
+            Self::RandomMetrics(inner) => Source::RandomMetrics(inner.build(sender)),
             Self::Sysinfo(inner) => Source::Sysinfo(inner.build(sender)),
-            Self::Timer(inner) => Source::Timer(inner.build(sender)),
         }
     }
 }
